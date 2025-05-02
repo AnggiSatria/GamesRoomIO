af116a4bc6ad2802956f42a2e3d457089b1ee37e
AnggiSatria <anggisatria122@gmail.com>
2025-05-02 23:54:33 +0700

    fix(master): revision remove localhost 3000

--- src/shared/config/axios.ts
+++ src/shared/config/axios.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Origin": `${process.env.NEXT_PUBLIC_API_URL}`,
     "ngrok-skip-browser-warning": "true",
   },
 });
